8c223ca75cc9f408b5e990969bc189700dcfaa78
kuma <kumadesuga0427@gmail.com>
2016-12-07 23:48:31 +0900

    make reducer to parse mark down text

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,3 +1,9 @@
-/**
- * Created by kuma on 2016/12/07.
- */
+import {PARSE_MARK_DOWN_TEXT} from '../actions/actions'
+var marked = require('marked');
+
+function  markDownText(state = '', action){
+	switch(action.type){
+		case PARSE_MARK_DOWN_TEXT:
+			return marked(state)
+	}
+}
