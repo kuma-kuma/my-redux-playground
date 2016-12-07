ba81daddcd692d2c990e5c6e3e2f0ef0b33fb821
kuma <kumadesuga0427@gmail.com>
2016-12-07 23:37:55 +0900

    make action to parse MarkDown text

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,5 @@
-/**
- * Created by kuma on 2016/12/07.
- */
+export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT'
+
+export function parseMarkDownText(text){
+	return { type: PARSE_MARK_DOWN_TEXT, text}
+}
