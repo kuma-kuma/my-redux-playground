e1ed63ead2dd23d9e8dab1718c76455e5cd619cb
kuma <0427kumadesuga@gmail.com>
2016-12-08 23:07:26 +0900

    rename argument of parseMarkDownText to id

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,8 @@
 export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT';
 export const ADD_MEMO = 'ADD_MEMO';
 
-export function parseMarkDownText(id){
-	return { type: PARSE_MARK_DOWN_TEXT, id}
+export function parseMarkDownText(index){
+	return { type: PARSE_MARK_DOWN_TEXT, index}
 }
 
 export function addMemo(title){
