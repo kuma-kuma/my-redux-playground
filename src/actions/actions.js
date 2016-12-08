d99bc706e84d1bbc46d7dce7b7deecafadcc7ba4
kuma <0427kumadesuga@gmail.com>
2016-12-08 22:02:38 +0900

    make ADD_MEMO action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,10 @@
-export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT'
+export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT';
+export const ADD_MEMO = 'ADD_MEMO';
 
 export function parseMarkDownText(text){
 	return { type: PARSE_MARK_DOWN_TEXT, text}
+}
+
+export function addMemo(title){
+	return { type: ADD_MEMO, title}
 }
