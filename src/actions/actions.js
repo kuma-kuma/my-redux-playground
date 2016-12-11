0ad38bc3069db49bb041b87822663d542e345e42
kuma <0427kumadesuga@gmail.com>
2016-12-11 11:12:54 +0900

    restructure function to arrow function

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,10 @@
 export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT';
 export const ADD_MEMO = 'ADD_MEMO';
 
-export function parseMarkDownText(index){
+export const parseMarkDownText = (index) => {
 	return { type: PARSE_MARK_DOWN_TEXT, index}
 }
 
-export function addMemo(title, id){
+export const addMemo = (title, id) => {
 	return { type: ADD_MEMO, title, id}
 }
