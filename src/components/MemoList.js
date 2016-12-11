adaf0502ae7d7b4026ea621b83929c96990e9afa
kuma <0427kumadesuga@gmail.com>
2016-12-12 08:00:32 +0900

    add onMemoClick to props

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -13,4 +13,13 @@ const MemoList = ({memos}) => (
 	</div>
 );
 
+MemoList.propTypes = {
+	memos: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		text: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired
+	}).isRequired).isRequired,
+	onMemoClick: PropTypes.func.isRequired
+}
+
 export default MemoList;
--- src/containers/MemoListContainer.js
+++ src/containers/MemoListContainer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import {connect} from "react-redux";
 import MemoList from "../components/MemoList";
+import {displayMemo} from "../actions/actions";
 
 const mapStateToProps = state => {
 	return state
 };
 
+const mapDispatchToProps = dispatch => {
+	return {
+		onMemoClick: (id) => {
+			dispatch(displayMemo(id));
+		}
+	}
+}
+
 const MemoListContainer = connect(
-	mapStateToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(MemoList);
 
 export default MemoListContainer
