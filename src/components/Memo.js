fc2cd38db43953035c3f7323ad947d24a27f983b
kuma <0427kumadesuga@gmail.com>
2016-12-11 17:46:21 +0900

    create callback to update memo text when text is modified.

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,14 +1,25 @@
 import React, {PropTypes} from "react";
 
-const Memo = ({text, title}) => (
+const Memo = ({text, title, onChange, id}) => (
 	<div>
 		<textarea>{title}</textarea>
-		<textarea>{text}</textarea>
+		<textarea
+			onChange={e => {
+				e.preventDefault()
+				onChange(e.target.value, id)
+			}}
+		>
+
+			{text}
+
+		</textarea>
 	</div>
 );
 
 Memo.propTypes = {
-	text: PropTypes.string.isRequired
+	text: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+	id: PropTypes.number.isRequired
 };
 
 export default Memo;
