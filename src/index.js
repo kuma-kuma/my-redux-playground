de8d2e82b70e4240f70b8f5a55c1e45db434ced7
kuma <0427kumadesuga@gmail.com>
2016-12-11 14:06:22 +0900

    make index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import { parseMarkDownText, addMemo} from './actions/actions'
-import memoApp from'./reducers/reducers'
-import { createStore } from 'redux'
+import React from "react";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {render} from "react-dom";
+import {addMemo, modifyText} from "./actions/actions";
+import memoApp from "./reducers/reducers";
+import App from "./components/App";
 
-let store = createStore(memoApp)
+let store = createStore(memoApp);
+
+let unsubscribe = store.subscribe(() =>
+	console.log(store.getState())
+);
+
+store.dispatch(addMemo('aiu', 0));
+store.dispatch(addMemo('iue', 1));
+store.dispatch(modifyText('aiueo', 0));
+
+render(
+	<Provider store={store}>
+		<App/>
+	</Provider>,
+	document.getElementById('root')
+)
