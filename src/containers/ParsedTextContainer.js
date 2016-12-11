03d8e2f32d6df74ee96a3bcdee959c0a8b7e59d5
kuma <0427kumadesuga@gmail.com>
2016-12-11 17:06:09 +0900

    add process to parse mark down text.

--- src/containers/ParsedTextContainer.js
+++ src/containers/ParsedTextContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import {connect} from "react-redux";
 import ParsedText from "../components/ParsedText";
+import {NON_DISPLAYED_MEMO} from "../actions/actions";
 
 const marked = require('marked');
 
-const mapStateToProps = (state, id) => {
-	return parseMarkDown(state.memos[id].text)
+const mapStateToProps = (state) => {
+	switch (state.displayedMemoId) {
+		case NON_DISPLAYED_MEMO:
+			return {text: ''};
+		default:
+			return {text: parseMarkDown(state.memos[state.displayedMemoId].text)}
+	}
 };
 
 const parseMarkDown = (text) => {
