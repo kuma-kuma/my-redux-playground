fe4838afe10efd4116afc47cd2e083262c025e51
kuma <0427kumadesuga@gmail.com>
2016-12-13 08:28:13 +0900

    add MemoContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MemoListContainer from "../containers/MemoListContainer";
 import ParsedTextContainer from "../containers/ParsedTextContainer";
+import MemoContainer from "../containers/MemoContainer";
 
 const App = () => (
 	<div>
 		<MemoListContainer/>
+		<MemoContainer/>
+
 		<ParsedTextContainer/>
 	</div>
 );
