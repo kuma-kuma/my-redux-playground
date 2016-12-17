a3d7f72ee7337176ed948395e7f4e3579171bf47
kuma <0427kumadesuga@gmail.com>
2016-12-17 17:01:41 +0900

    add test for handling DELETE_MEMO action

--- __test__/reducers/directory.spec.js
+++ __test__/reducers/directory.spec.js
@@ -31,7 +31,35 @@ describe('directory', () => {
 	});
 
 	it('should handle delete memo', () => {
-
+		expect(
+			reducer(
+				{
+					4: {
+						path: '4',
+						id: 4,
+						text: 'test',
+						title: 'title4'
+					},
+					5: {
+						path: '5',
+						id: 5,
+						text: 'test',
+						title: 'title5'
+					}
+				},
+				{
+					type: types.DELETE_MEMO,
+					id: 4
+				}
+			)
+		).toEqual({
+			5: {
+				path: '5',
+				id: 5,
+				text: 'test',
+				title: 'title5'
+			}
+		})
 	});
 
 	it('should handle save memo', () => {
