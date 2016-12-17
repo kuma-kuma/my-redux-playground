bdabc5e7d8987388119e698752495bb3ad91abcf
kuma <0427kumadesuga@gmail.com>
2016-12-17 16:27:40 +0900

    add test for handling ADD_MEMO action

--- __test__/reducers/directory.spec.js
+++ __test__/reducers/directory.spec.js
@@ -1,6 +1,33 @@
-describe('directory', () => {
-	it('should handle add memo', () => {
+import reducer from "../../src/reducers/directory";
+import * as types from "../../src/actions/actions";
 
+describe('directory', () => {
+
+	it('should return the initial state', () => {
+		expect(reducer(undefined, {})).toBe({});
+	});
+
+	it('should handle add memo', () => {
+		const id = 2;
+		const path = '/dir1/' + id;
+
+		expect(
+			reducer(
+				{},
+				{
+					type: types.ADD_MEMO,
+					path: path,
+					id: id
+				}
+			)
+		).toEqual({
+			2: {
+				path: path,
+				id: id,
+				text: '',
+				title: '',
+			}
+		});
 	});
 
 	it('should handle delete memo', () => {
