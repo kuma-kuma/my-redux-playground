485736a6a800c904de23aa528486efd944655237
kuma <0427kumadesuga@gmail.com>
2016-12-17 17:05:24 +0900

    create function to delete memo or directory

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,7 @@ export const PARSE_MARK_DOWN_TEXT = 'PARSE_MARK_DOWN_TEXT';
 export const ADD_MEMO = 'ADD_MEMO';
 export const MODIFY_TEXT = 'MODIFY_TEXT';
 export const DISPLAY_MEMO = 'DISPLAY_MEMO';
-export const DELETE_MEMO = 'DELETE_MEMO';
+export const DELETE_MEMO_OR_DIRECTORY = 'DELETE_MEMO_OR_DIRECTORY';
 
 export const NON_DISPLAYED_MEMO = -1;
 
@@ -20,4 +20,8 @@ export const modifyText = (id, text) => {
 
 export const displayMemo = (id) => {
 	return {type: DISPLAY_MEMO, id}
+};
+
+export const deleteMemoOrDirectory = (id) => {
+	return {type: DELETE_MEMO_OR_DIRECTORY, id}
 };
