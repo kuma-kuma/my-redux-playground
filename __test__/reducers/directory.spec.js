fb9c340e1dcb5eca7dfdfecce9c814104cd7545b
kuma <0427kumadesuga@gmail.com>
2016-12-17 09:56:50 +0900

    create skeleton structure of test of directory.js

---
+++ __test__/reducers/directory.spec.js
@@ -0,0 +1,17 @@
+describe('directory', () => {
+	it('should handle add memo', () => {
+
+	});
+
+	it('should handle delete memo', () => {
+
+	});
+
+	it('should handle save memo', () => {
+
+	});
+
+	it('should handle update memo', () => {
+
+	});
+});
