e55e817977fedea92d0944cd8abe1b7ad4b14844
kuma <0427kumadesuga@gmail.com>
2016-12-18 09:03:19 +0900

    add case of handling DELETE_MEMO_OR_DIRECTORY

--- src/reducers/directory.js
+++ src/reducers/directory.js
@@ -1,4 +1,5 @@
-import {ADD_MEMO} from "../actions/actions";
+import {ADD_MEMO, DELETE_MEMO_OR_DIRECTORY} from "../actions/actions";
+import _ from "lodash";
 
 const fileStructure = (state = {}, action) => {
 	switch (action.type) {
@@ -6,6 +7,10 @@ const fileStructure = (state = {}, action) => {
 			let obj = {...state};
 			obj[action.id] = memo(undefined, action);
 			return obj;
+		case DELETE_MEMO_OR_DIRECTORY:
+			return _.pickBy(state, (o) => {
+				return o.id !== action.id;
+			});
 		default:
 			return state;
 	}
@@ -23,8 +28,6 @@ function memo(state = {}, action) {
 		default:
 			return state;
 	}
-
 }
-
 
 export default fileStructure;
