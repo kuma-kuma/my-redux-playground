bb3ea1fd029700ca807ceb2f56f457f1bfb0c0d4
kuma <0427kumadesuga@gmail.com>
2016-12-18 10:12:18 +0900

    create function to get mock directory

--- __test__/mock/mockState.js
+++ __test__/mock/mockState.js
@@ -7,38 +7,13 @@ const getMockMemo = (id, text, title, path) => {
 	}
 };
 
-function getMockDirectory() {
+const getMockDirectory = (id, title, path) => {
 	return {
-		1: {
-			path: '/dir1/1',
-			title: 'title1',
-			text: 'text1',
-			id: 1
-		},
-		dir1: {
-			path: '/dir1',
-			title: 'title',
-			id: 'dir1'
-		},
-		3: {
-			path: '/dir1/3',
-			title: 'title3',
-			text: 'text3',
-			id: 3
-		},
-		4: {
-			path: '/dir1/4',
-			title: 'title4',
-			text: 'text4',
-			id: 4
-		},
-
-		dir2: {
-			path: '/dir2',
-			title: 'dir2',
-			id: 'dir2'
-		},
+		id: id,
+		title: title,
+		path: path
 	}
-}
+};
 
 export default getMockMemo;
+export default getMockDirectory;
