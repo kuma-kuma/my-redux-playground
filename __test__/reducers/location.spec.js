f4afc94e5f59f71cd06b766af4e9a36deaaa45c3
kuma <0427kumadesuga@gmail.com>
2016-12-26 23:38:33 +0900

    create test of making sure state is initialized

--- __test__/reducers/location.spec.js
+++ __test__/reducers/location.spec.js
@@ -1,6 +1,7 @@
-descirbe('location', () => {
+import reducer from "../../src/reducers/location";
+describe('location', () => {
 	it('should return initial state', () => {
-
+		expect(reducer(undefined, {type: ''})).toBe('/')
 	});
 
 	it('should handle CHANGE_LOCATION', () => {
