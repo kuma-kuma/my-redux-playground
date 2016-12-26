a4ab59c1b0e58241d71b2793c50013f8b76bf502
kuma <0427kumadesuga@gmail.com>
2016-12-26 23:43:51 +0900

    implement test of handling CHANGE_LOCATION.

--- __test__/reducers/location.spec.js
+++ __test__/reducers/location.spec.js
@@ -1,9 +1,21 @@
 import reducer from "../../src/reducers/location";
+import * as types from "../../src/actions/actions";
+
 describe('location', () => {
 	it('should return initial state', () => {
 		expect(reducer(undefined, {type: ''})).toBe('/')
 	});
 
 	it('should handle CHANGE_LOCATION', () => {
+		const location = '/1/2/';
+		expect(
+			reducer(
+				'/',
+				{
+					type: types.CHANGE_LOCATION,
+					location: location
+				}
+			)
+		).toBe(location);
 	})
 });
