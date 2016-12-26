18418f80f884f5c876f067422212f6e5a0d289e4
kuma <0427kumadesuga@gmail.com>
2016-12-26 23:44:18 +0900

    handle CHANGE_LOCATION

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -1,5 +1,9 @@
+import {CHANGE_LOCATION} from "../actions/actions";
+
 const location = (state = '/', action) => {
 	switch (action.type) {
+		case CHANGE_LOCATION:
+			return action.location;
 		default:
 			return state;
 	}
