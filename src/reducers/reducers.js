b452fdcd8789ca1d0a761d5fe65356a2a88151a8
kuma <0427kumadesuga@gmail.com>
2016-12-28 01:38:48 +0900

    add directories reducer to root reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import displayedMemoId from "./displayedMemoId";
 import memos from "./memos";
+import directories from "./directory";
 import location from "./location";
 
 const memoApp = combineReducers({
 	memos,
+	directories,
 	displayedMemoId,
 	location
 });
