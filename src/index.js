e3220d1d290dd239ebc45bc5baed98e5ab88594f
kuma <0427kumadesuga@gmail.com>
2016-12-30 14:20:28 +0900

    change for debugging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {render} from "react-dom";
-import {addMemo, modifyMemo, displayMemo} from "./actions/actions";
+import {addMemo, modifyMemo, displayMemo, changeLocation, addDirectory} from "./actions/actions";
 import memoApp from "./reducers/reducers";
 import App from "./components/App";
 
@@ -12,10 +12,13 @@ let unsubscribe = store.subscribe(() =>
 	console.log(store.getState())
 );
 
-store.dispatch(addMemo(0, 'aiu'));
-store.dispatch(addMemo(1, 'iue'));
+store.dispatch(addMemo(0, 'aiu', '/'));
+store.dispatch(addMemo(1, 'iue', '/'));
+store.dispatch(addDirectory(2, '/', 'dir1'));
+store.dispatch(addDirectory(3, '/2/', 'dir2'));
 store.dispatch(modifyMemo(0, '# aiueo'));
 store.dispatch(displayMemo(0));
+store.dispatch(changeLocation('/2/'));
 
 render(
 	<Provider store={store}>
