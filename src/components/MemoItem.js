94218423308ae8b9a67eff3cbf42e7244a3881d2
kuma <0427kumadesuga@gmail.com>
2016-12-31 23:50:08 +0900

    dispatch action when memo item is clicked

--- src/components/MemoItem.js
+++ src/components/MemoItem.js
@@ -1,9 +1,14 @@
 import React, {PropTypes} from "react";
+import {connect} from "react-redux";
+import {displayMemo} from "../actions/actions";
 
-const MemoItem = ({item}) => (
-	<div>
+const MemoItem = ({item, dispatch}) => (
+	<div onClick={e => {
+		e.preventDefault();
+		dispatch(displayMemo(item.id));
+	}}>
 		<p>{item.title}</p>
 	</div>
 );
 
-export default MemoItem;
+export default connect()(MemoItem);
